8d0d98f7aa37a6d4d57978944ab633dde355d560
Matt Robitaille <matt@anchour.com>
2023-05-08 23:44:08 -0400

    Additional UX improvements for CopyComponent.jsx

--- src/scripts/components/CopyComponent.jsx
+++ src/scripts/components/CopyComponent.jsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from 'preact/hooks'
 export default function CopyComponent({ onClick, children = 'Copy code' }) {
+  const defaultText = children;
+  const [clicked, setClicked] = useState(false)
+  const [text, setText] = useState(children)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setClicked(false)
+      setText(defaultText)
+    }, 5000)
+  }, [clicked])
+
+  const elProps = {
+    type: 'button',
+    onClick: () => {
+      setClicked(true)
+      setText('Copied!')
+      onClick()
+    },
+    className: 'rounded text-xs uppercase tracking-wider leading-none bg-slate-800 px-4 py-2 text-white font-bold absolute -top-4 -right-4 [&[data-open="false"]]:opacity-0 [&[data-open="false"]]:transform [&[data-open="false"]]:scale-95 [&[data-open="false"]]:transition-all group-hover:opacity-100 group-hover:scale-100 border-2 border-smoke hover:bg-water hover:text-white',
+    ['data-open']: clicked,
+  }
+
   return (
-    <button type='button' onClick={onClick} className='rounded text-xs uppercase tracking-wider leading-none bg-slate-800 px-4 py-2 text-white font-bold absolute -top-4 -right-4 opacity-0 transform scale-95 transition-all group-hover:opacity-100 group-hover:scale-100 border-2 border-smoke hover:bg-water hover:text-white'>{children}</button>
+    <button {...elProps}>{text}</button>
   )
 }
