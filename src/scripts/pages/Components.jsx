61d8961c05101d9702bdeaa8904504d7b75f1ede
Matt Robitaille <matt@anchour.com>
2023-05-11 15:36:28 -0400

    Components page TOC

--- src/scripts/pages/Components.jsx
+++ src/scripts/pages/Components.jsx
@@ -5,16 +5,38 @@ import Typography from '../components/Typography.jsx'
 export default function Components() {
   return (
     <>
-      <div className="container">
+      {/* Grid layout with 200px wide layout on left for Table of Contents */}
+      <div className="grid grid-cols-1 md:grid-cols-[300px,1fr]">
 
-        <Accordion header="Typography" open>
-          <Typography />
-        </Accordion>
 
-        <Accordion header="Buttons" open>
-          <Buttons />
-        </Accordion >
-      </div >
+        {/* Table of Contents */}
+
+        <div className="sticky top-0 h-screen p-4 overflow-y-auto bg-white">
+          <div className="mb-4 text-xl font-semibold tracking-wider uppercase border-b border-b-black">Table of Contents</div>
+          <ul className="list-none">
+            <li className="py-1">
+              <a href="#Typography">Typography</a>
+            </li>
+
+            <li className="py-1">
+
+              <a href="#Buttons">Buttons</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="container">
+          <h3 id="Typography">Typography</h3>
+          <Accordion header="Typography" open>
+            <Typography />
+          </Accordion>
+
+          <h3 id="Buttons">Buttons</h3>
+          <Accordion header="Buttons" open>
+            <Buttons />
+          </Accordion >
+        </div >
+      </div>
     </>
   )
 }
