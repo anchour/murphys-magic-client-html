b03256fe9a1ddf016112c19f38df149ca98ede4d
Matt Robitaille <matt@anchour.com>
2023-05-12 11:49:41 -0400

    General tailwind work.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,28 @@ export default {
   theme: {
     extend: {
       colors: {
-        water: "#0a4de2",
-        smoke: "#2b2b2b",
+        // Primary
+        smoke: `var(--color-primary-smoke)`,
+        bone: `var(--color-primary-bone)`,
+        dove: `var(--color-primary-dove)`,
+
+        // Secondary
+        water: `var(--color-secondary-water)`,
+        green: `var(--color-secondary-light)`,
+        earth: `var(--color-secondary-earth)`,
       },
+      
+      fontFamily: {
+        sans: [`var(--font-sans)`],
+        heading: [`var(--font-heading)`],
+      },
+
       container: {
         center: true,
+        padding: {
+          DEFAULT: `1.5rem`,
+          sm: `2rem`,
+        }
       },
     },
   },
