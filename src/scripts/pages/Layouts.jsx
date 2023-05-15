3dc3ce1a3e370f3e3953d26a2355791f5090bdf4
Matt Robitaille <matt@anchour.com>
2023-05-15 17:40:28 -0400

    Missing import

--- src/scripts/pages/Layouts.jsx
+++ src/scripts/pages/Layouts.jsx
@@ -1,6 +1,7 @@
 import SuperHeader from '../components/SuperHeader';
 import CollectionGrid from '../components/CollectionGrid';
 import Header from '../components/Header';
+import TextImageSplit from '../components/TextImageSplit';
 
 export default function Layouts() {
   return <>
