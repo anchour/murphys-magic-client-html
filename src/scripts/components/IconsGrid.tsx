e328fece00c5be839ed763be9e6b61cfe1ea3d6a
Rory McGuire <rory@anchour.com>
2023-05-18 11:34:44 -0400

    - Fix file outputs - Adjust heading on Icon grid

--- src/scripts/components/IconsGrid.tsx
+++ src/scripts/components/IconsGrid.tsx
@@ -1,4 +1,8 @@
 import { Icon } from "./Icons";
+import Top from "/src/images/icons/ico-top_magicians.svg";
+import Quality from "/src/images/icons/ico-superior_quality.svg";
+import USA from "/src/images/icons/ico-USA.svg";
+import Learn from "/src/images/icons/ico-learn.svg";
 
 export default function IconGrid() {
   return (
@@ -9,22 +13,22 @@ export default function IconGrid() {
 
           <div className="icons-grid__icons">
             <Icon
-              icon="../src/images/icons/ico-top_magicians.svg"
+              icon={Top}
               description="Top Magicians"
               showCopyComponent={false}
             />
             <Icon
-              icon="../src/images/icons/ico-superior_quality.svg"
+               icon={Quality}
               description="Superior Quality"
               showCopyComponent={false}
             />
             <Icon
-              icon="../src/images/icons/ico-USA.svg"
+               icon={USA}
               description="Designed in USA"
               showCopyComponent={false}
             />
             <Icon
-              icon="../src/images/icons/ico-learn.svg"
+              icon={Learn}
               description="Easy to Learn"
               showCopyComponent={false}
             />
