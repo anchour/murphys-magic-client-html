d016d26011fd485dc2fea5e5a553a64b7ab1a1fb
Matt Robitaille <matt@anchour.com>
2023-05-18 11:10:08 -0400

    Wrap collection hero

--- src/scripts/components/Breadcrumbs.tsx
+++ src/scripts/components/Breadcrumbs.tsx
@@ -10,35 +10,31 @@ interface BreadcrumbsProps {
 const Breadcrumbs = ({ excludeHomeLink = false }: BreadcrumbsProps) => {
   return <>
     <nav className="breadcrumb" aria-label="Breadcrumb">
-      <ol role="list" className="flex items-center space-x-4">
+      <ol role="list" className="breadcrumb__list">
         {!excludeHomeLink &&
-          <li>
-            <div>
-              <a href="#" className="text-gray-400 hover:text-gray-500">
-                Home
-              </a>
-            </div>
+          <li className="breadcrumb__item">
+            <a href="#" className="breadcrumb__link">
+              Home
+            </a>
           </li>}
         {pages.map((page, index) => (
-          <li key={page.name}>
-            <div className="breadcrumb__item">
-              {excludeHomeLink && index > 0 &&
-                <svg
-                  className="hero__breadcrumb__delimiter"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  aria-hidden="true"
-                >
-                  <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
-                </svg>}
-              <a
-                href={page.href}
-                className=""
-                aria-current={page.current ? 'page' : undefined}
+          <li key={page.name} className="breadcrumb__item">
+            {excludeHomeLink && index > 0 &&
+              <svg
+                className="hero__breadcrumb__delimiter"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                aria-hidden="true"
               >
-                {page.name}
-              </a>
-            </div>
+                <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
+              </svg>}
+            <a
+              href={page.href}
+              className="breadcrumb__link"
+              aria-current={page.current ? 'page' : undefined}
+            >
+              {page.name}
+            </a>
           </li>
         ))}
       </ol>
