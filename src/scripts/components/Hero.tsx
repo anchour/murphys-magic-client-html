0ad457421384a30992bb3b9580836dbd9eb5c394
Rory McGuire <rory@anchour.com>
2023-05-18 14:40:17 -0400

    - Light & Dark Hero - Small adjustments to Collections hero

---
+++ src/scripts/components/Hero.tsx
@@ -0,0 +1,39 @@
+
+import LightHeroBG from './CollectionHeroBG.svg.tsx';
+import DarkHeroBG from '../../images/hero--dark-background.jpg';
+
+interface HeroProps {
+  title?: string, breadcrumbs?: null | JSX.Element, description?: string, style?: string
+}
+
+const Hero = ({ title = "Murphy's Magic", breadcrumbs = null, description = "Where the fun comes free", style="light" }: HeroProps) => {
+  return <>
+    <section className={'hero hero--page hero--' + style}>
+      <div className="hero__inner">
+
+        <div className="hero__background">
+          {style === "dark" ?
+            <img className="hero__background--image" src={DarkHeroBG} alt="Become a Dealer" width={1789} height={1289} loading='lazy' />
+           : 
+           <LightHeroBG />}
+        </div>
+        
+        <div className="container">
+          <h1 className="hero__title typography-heading typography-heading-lg">
+            {title}
+          </h1>
+
+          {
+            breadcrumbs &&
+            <div className="hero__breadcrumbs">
+              {breadcrumbs}
+            </div>
+          }
+
+          {description && <p className="hero__description typography-body-md"> {description} </p>}
+        </div>
+      </div>
+    </section>
+  </>
+}
+export default Hero
--- src/scripts/pages/Layouts.jsx
+++ src/scripts/pages/Layouts.jsx
@@ -5,6 +5,7 @@ import TextImageSplit from '../components/TextImageSplit';
 import BenefitsGrid from '../components/BenefitsGrid';
 import CollectionHero from '../components/CollectionHero';
 import Breadcrumbs from '../components/Breadcrumbs';
+import Hero from '../components/Hero';
 
 export default function Layouts() {
   return <>
@@ -17,5 +18,7 @@ export default function Layouts() {
     <CollectionGrid />
     <BenefitsGrid />
     <CollectionHero breadcrumbs={<Breadcrumbs excludeHomeLink />} />
+    <Hero title="Become a Dealer" description="Grow your business, change the world" style="dark"/>
+    <Hero />
   </>
 }
