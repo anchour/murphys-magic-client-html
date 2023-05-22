a82703644e0c9adb4715de52d670d200c1d53f73
Matt Robitaille <matt@anchour.com>
2023-05-22 17:25:10 -0400

    Brand header section/layout setup.

---
+++ src/scripts/components/graphics/DropGraphic.tsx
@@ -0,0 +1,35 @@
+export default function DropGraphic() {
+  return (
+    <svg width="223" height="277" viewBox="0 0 223 277" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M110.812 243.628C90.0785 248.463 30.5312 233.882 30.5312 179.64C30.5312 109.181 126.258 0.452881 126.258 0.452881C126.258 0.452881 183.379 76.8766 198.825 103.894C214.272 130.911 239.539 182.199 205.768 237.457C174.517 288.594 82.8538 275.405 82.8538 275.405C82.8538 275.405 190.321 262.16 194.949 195.764C199.578 129.368 126.973 44.4596 126.973 44.4596C126.973 44.4596 72.2049 129.387 66.0338 147.129C36.5518 231.041 110.812 243.628 110.812 243.628Z" fill="url(#paint0_linear_548_25545)" />
+      <path d="M96.7763 0.452881C96.7763 0.452881 -2.281 124.364 0.559961 182.933C3.41973 241.521 51.5279 275.574 87.2375 275.574C122.947 275.574 189.267 261.464 194.93 195.764C200.594 130.065 126.954 44.4596 126.954 44.4596L111.301 69.0875C117.51 77.6856 123.493 86.4719 129.344 95.3334C139.541 110.761 150.736 126.885 157.208 144.307C161.234 155.144 163.153 166.79 162.739 178.361C162.081 196.611 156.041 217.062 142.232 229.63C134.198 236.949 124.038 241.558 113.371 243.383C91.4895 247.108 67.934 239.62 51.8478 224.418C32.7136 206.338 27.8219 179.734 34.6139 154.862C36.1943 149.086 38.3015 143.46 40.7849 138.023C64.5474 86.0768 126.221 0.452881 126.221 0.452881H96.7575H96.7763Z" fill="url(#paint1_linear_548_25545)" />
+      <defs>
+        <linearGradient id="paint0_linear_548_25545" x1="126.503" y1="0.452881" x2="126.503" y2="276.929" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#DCD5C8" />
+          <stop offset="0.03" stop-color="#D6CDBF" />
+          <stop offset="0.19" stop-color="#B6A189" />
+          <stop offset="0.34" stop-color="#9F8161" />
+          <stop offset="0.46" stop-color="#916E49" />
+          <stop offset="0.53" stop-color="#8C6741" />
+          <stop offset="0.63" stop-color="#7B5C3D" />
+          <stop offset="0.83" stop-color="#514233" />
+          <stop offset="0.98" stop-color="#2B2B2B" />
+        </linearGradient>
+        <linearGradient id="paint1_linear_548_25545" x1="97.8863" y1="275.574" x2="97.8863" y2="0.452881" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#DCD5C8" />
+          <stop offset="0.03" stop-color="#D6CDBF" />
+          <stop offset="0.2" stop-color="#B6A189" />
+          <stop offset="0.35" stop-color="#9F8161" />
+          <stop offset="0.47" stop-color="#916E49" />
+          <stop offset="0.54" stop-color="#8C6741" />
+          <stop offset="0.62" stop-color="#6D543A" />
+          <stop offset="0.71" stop-color="#504233" />
+          <stop offset="0.8" stop-color="#3B352E" />
+          <stop offset="0.9" stop-color="#2F2D2B" />
+          <stop offset="0.98" stop-color="#2B2B2B" />
+        </linearGradient>
+      </defs>
+    </svg>
+
+  )
+}
