8cc5837562f57c2bc11110fae34a60b80ae2dda3
Matt Robitaille <matt@anchour.com>
2023-05-22 16:48:30 -0400

    Fix grid layout on mobile. Negative margin for container.

--- src/scripts/components/CalloutGrid.tsx
+++ src/scripts/components/CalloutGrid.tsx
@@ -9,38 +9,40 @@ interface CalloutGridProps {
 
 const CalloutGrid = (props: CalloutGridProps) => {
   return <section className="page-section callout-grid">
-    <div className="container callout-grid__grid">
-      <a href="#" className="callout-grid__area callout-grid__area--primary">
-        <img src={Primary} loading='lazy' width={1348} height={1661} alt='Toy & Hobby Collection' />
+    <div className="container">
+      <div className='callout-grid__grid'>
+        <a href="#" className="callout-grid__area callout-grid__area--primary">
+          <img src={Primary} loading='lazy' width={1348} height={1661} alt='Toy & Hobby Collection' />
 
-        <div className="callout-grid__area__title typography-heading-xs">
-          Toy & hobby collection <ArrowRight />
-        </div>
-      </a>
+          <div className="callout-grid__area__title typography-heading-xs">
+            Toy & hobby collection <ArrowRight />
+          </div>
+        </a>
 
-      <a href="#" className="callout-grid__area callout-grid__area--secondary">
-        <img src={Secondary} loading='lazy' width={1380} height={813} alt='Cult Classics' />
+        <a href="#" className="callout-grid__area callout-grid__area--secondary">
+          <img src={Secondary} loading='lazy' width={1380} height={813} alt='Cult Classics' />
 
-        <div className="callout-grid__area__title typography-heading-xs">
-          Cult Classics <ArrowRight />
-        </div>
-      </a>
+          <div className="callout-grid__area__title typography-heading-xs">
+            Cult Classics <ArrowRight />
+          </div>
+        </a>
 
-      <a href="#" className="callout-grid__area callout-grid__area--tertiary">
-        <img src={Tertiary} loading='lazy' width={674} height={813} alt='New Magicians Collection' />
+        <a href="#" className="callout-grid__area callout-grid__area--tertiary">
+          <img src={Tertiary} loading='lazy' width={674} height={813} alt='New Magicians Collection' />
 
-        <div className="callout-grid__area__title typography-heading-xs">
-          New Magicians Collection <ArrowRight />
-        </div>
-      </a>
+          <div className="callout-grid__area__title typography-heading-xs">
+            New Magicians Collection <ArrowRight />
+          </div>
+        </a>
 
-      <a href="#" className="callout-grid__area callout-grid__area--quaternary">
-        <img src={Quaternary} loading='lazy' width={674} height={813} alt='Best Sellers' />
+        <a href="#" className="callout-grid__area callout-grid__area--quaternary">
+          <img src={Quaternary} loading='lazy' width={674} height={813} alt='Best Sellers' />
 
-        <div className="callout-grid__area__title typography-heading-xs">
-          Best Sellers <ArrowRight />
-        </div>
-      </a>
+          <div className="callout-grid__area__title typography-heading-xs">
+            Best Sellers <ArrowRight />
+          </div>
+        </a>
+      </div>
     </div>
   </section>
 }
