999a789c1d4a4871bcb322fab0dbca33d5b919a8
Rory McGuire <rory@anchour.com>
2023-05-23 14:31:45 -0400

    - Build Become a Dealer Page

--- src/scripts/pages/BecomeADealer.tsx
+++ src/scripts/pages/BecomeADealer.tsx
@@ -1,24 +1,42 @@
-import BenefitsGrid from "../components/BenefitsGrid"
-import Header from "../components/Header"
-import MailingListSignup from "../components/MailingListSignup"
-import SuperHeader from "../components/SuperHeader"
-import TextCallout from "../components/TextCallout"
+import BenefitsGrid from "../components/BenefitsGrid";
+import Header from "../components/Header";
+import MailingListSignup from "../components/MailingListSignup";
+import SuperHeader from "../components/SuperHeader";
+import TextCallout from "../components/TextCallout";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
 
-interface BecomeADealerPageProps {
-}
+interface BecomeADealerPageProps {}
 
 const BecomeADealerPage = (props: BecomeADealerPageProps) => {
-  return <>
-    <SuperHeader />
-    <Header />
+  return (
+    <>
+      <SuperHeader />
+      <Header />
 
-    <main role="main">
-      <TextCallout />
-      <BenefitsGrid />
-    </main >
-    
-    <MailingListSignup />
-  </>
-}
+      <main role="main">
+        <Hero
+          title="Become a Dealer"
+          description="Grow your business, change the world"
+          style="dark"
+        />
+        <TextCallout />
+        <Hero
+          title="Bridging the ordinary with the extraordinary"
+          description="For 40+ years Murphy’s has been teaming up with Dealers to bring mystery and magic to the masses."
+          style="dark"
+          alignment="left"
+          hasCTA
+          headingLevel="h2"
+          buttonText="Apply now"
+        />
+        <BenefitsGrid />
+      </main>
 
-export default BecomeADealerPage
+      <MailingListSignup />
+      <Footer />
+    </>
+  );
+};
+
+export default BecomeADealerPage;
