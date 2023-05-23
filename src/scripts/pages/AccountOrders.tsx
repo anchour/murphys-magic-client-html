17b592a86b3e693977f241dcc16ca007d5f71cb7
Matt Robitaille <matt@anchour.com>
2023-05-23 11:48:17 -0400

    Orders page "wrap", pending QA.

--- src/scripts/pages/AccountOrders.tsx
+++ src/scripts/pages/AccountOrders.tsx
@@ -1,6 +1,6 @@
 import Details from "../components/Details"
 import ordersData from '../../scripts/data/orders.json';
-import { useState } from "react";
+import slugify from "slugify";
 
 interface AccountOrdersProps {
 }
@@ -21,6 +21,7 @@ interface OrderProps {
   orderNumber: string,
   invoiceLink?: string
   detailsLink?: string,
+  trackingLink?: string,
   editLink?: string,
   lineItems: LineItem[]
 }
@@ -29,11 +30,13 @@ const LineItem = (props: LineItem) => {
   const { title, quantity, image, price } = props;
 
   return <div className="line-item">
-    <div className="line-item__image">
-      <img src={image} alt={title} />
+    <a className="line-item__image" href="#"><img src={image} alt={title} loading="lazy" width={100} height={100} /></a>
+
+    <div>
+      <div className="line-item__title"><a href="#" title={`View ${title} in the store`}>{title}</a></div>
+      <div className="line-item__stat">Quantity: {quantity}</div>
+      <div className="line-item__stat">Price: ${(price / 100).toFixed(2)}</div>
     </div>
-    <div className="line-item__title">{title}</div>
-    <div className="line-item__quantity">{quantity}</div>
   </div>
 
 }
@@ -50,21 +53,22 @@ const Order = (props: OrderProps) => {
     detailsLink,
     editLink,
     lineItems,
+    trackingLink,
   } = props;
 
   return <div className="order">
     <div className="order__header">
-      <div className="order-stat">
+      <div className="order-stat order__date">
         <div className="order-stat__header">Order placed</div>
         <div className="order-stat__value">{date} </div>
       </div>
 
-      <div className="order-stat">
+      <div className="order-stat order-stat--hidden-mobile">
         <div className="order-stat__header">Order</div>
         <div className="order-stat__value">{orderNumber}</div>
       </div>
 
-      <div className="order-stat">
+      <div className="order-stat order__subtotal">
         <div className="order-stat__header">Subtotal</div>
         <div className="order-stat__value">${(subtotal / 100).toFixed(2)}</div>
       </div>
@@ -90,6 +94,18 @@ const Order = (props: OrderProps) => {
         <LineItem {...props} key={index} />
       )}
     </div>
+
+    <div className="order__footer">
+      <div className="order__status">
+        Status:
+
+        <div className={`order__status__badge order__status__badge--${slugify(status).toLowerCase()}`}>
+          {status}
+        </div>
+
+        {trackingLink && <a href={trackingLink} target="_blank" className="order__tracking-link">Track this shipment</a>}
+      </div>
+    </div>
   </div >
 }
 
@@ -97,7 +113,8 @@ const AccountOrders = (props: AccountOrdersProps) => {
   return <section className="page-section account-orders">
     <div className="container">
       <div className="account-sidebar">
-        <Details title="Orders">
+
+        <Details title="Orders" summaryClassName="typography-heading-xs" open>
           <ul className="account-sidebar__list">
             <li><a href="#">Recent Orders</a></li>
             <li><a href="#">Open Orders</a></li>
@@ -108,6 +125,18 @@ const AccountOrders = (props: AccountOrdersProps) => {
           </ul>
         </Details>
 
+        {[
+          "Account details",
+          "Reports",
+          "Product updates",
+          "Payment",
+          "Invoices",
+          "Returns",
+          "Support",
+          "Resources",
+        ].map((title, index) =>
+          <Details title={title} key={index} summaryClassName="typography-heading-xs" />
+        )}
       </div>
 
       <div className="account-primary-content">
@@ -120,7 +149,7 @@ const AccountOrders = (props: AccountOrdersProps) => {
         </div>
       </div>
     </div>
-  </section>
+  </section >
 }
 
 export default AccountOrders
