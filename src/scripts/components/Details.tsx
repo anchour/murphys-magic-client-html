1934f6611e3728db7cd67892e17bef8ae2322c41
Matt Robitaille <matt@anchour.com>
2023-05-23 11:47:20 -0400

    Clean up accordion styling for sidebar.

--- src/scripts/components/Details.tsx
+++ src/scripts/components/Details.tsx
@@ -1,15 +1,38 @@
+import classNames from "classnames";
+
 interface DetailsProps {
   title: string,
-  children: any
+  children?: any,
+  open?: boolean,
+  summaryClassName?: string,
 }
 
+const Caret = () => <svg width="16" height="8" viewBox="0 0 16 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M1 1L8 7L15 1" stroke="#2B2B2B" />
+</svg>
+
+
 const Details = (props: DetailsProps) => {
-  if (!props.children) {
-    return null;
+  const detailsProps = {
+    className: classNames('accordion'),
+    open: props.open,
   }
-  return <details>
-    <summary>{props.title}</summary>
-    {props.children}
+
+  const summaryProps = {
+    className: classNames('accordion__title', props.summaryClassName),
+  }
+
+  return <details {...detailsProps}>
+    <summary {...summaryProps}>
+      {props.title} <div className="accordion__caret">
+        <Caret />
+      </div>
+    </summary>
+
+
+    <div className="accordion__content">
+      {props.children}
+    </div>
   </details>
 }
 
