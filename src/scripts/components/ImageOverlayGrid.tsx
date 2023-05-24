3b77d422c81bb9060e499074dd0d98774f94d643
Rory McGuire <rory@anchour.com>
2023-05-24 14:40:07 -0400

    - Image Overlay Component - Image Overlay Layout

---
+++ src/scripts/components/ImageOverlayGrid.tsx
@@ -0,0 +1,10 @@
+import ImageOverlay from "../components/ImageOverlayColumn";
+
+export default function ImageOverlayGrid() {
+  return (
+    <section className="image-overlay-grid">
+      <ImageOverlay />
+      <ImageOverlay />
+    </section>
+  );
+}
