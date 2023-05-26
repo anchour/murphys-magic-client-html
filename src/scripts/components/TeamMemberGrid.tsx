1e75ec5b1ec07a437600c9149508a22dab812f89
Rory McGuire <rory@anchour.com>
2023-05-26 15:21:13 -0400

    - Wrapped team layout

--- src/scripts/components/TeamMemberGrid.tsx
+++ src/scripts/components/TeamMemberGrid.tsx
@@ -3,15 +3,21 @@ import { times } from "lodash-es";
 
 export default function TeamMembersGrid() {
   return (
-    <section className="team-members-grid">
-      <div className="container team-members-grid__title">
-        <h2>The Murphy's team</h2>
-      </div>
-      
-      <div className="container team-members-grid__members">
-        {times(8, () => (
-          <TeamMember showCopyComponent={false} />
-        ))}
+    <section className="page-section--tall team-members-grid">
+      <div className="container">
+        <div className="team-members-grid__top-content">
+          <h2>The Murphy's team</h2>
+          <p className="typography-body-sm">
+            We strive for greatness in all aspects of our industry, and are
+            passionate about bringing the mystery of magic to the world.
+          </p>
+        </div>
+
+        <div className="team-members-grid__members">
+          {times(8, () => (
+            <TeamMember showCopyComponent={false} />
+          ))}
+        </div>
       </div>
     </section>
   );
