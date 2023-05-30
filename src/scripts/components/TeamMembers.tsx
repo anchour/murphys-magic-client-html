0616bd6de838f5e04993aa31471ab68dbc907fd3
Matt Robitaille <matt@anchour.com>
2023-05-30 15:46:55 -0400

    Remove unused TeamMembers component.

--- src/scripts/components/TeamMembers.tsx
+++ src/scripts/components/TeamMembers.tsx
@@ -69,14 +69,3 @@ export function TeamMember({
     </div>
   );
 }
-
-export default function TeamMembers() {
-  return (
-    <>
-      <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        <TeamMember />
-        <TeamMember />
-      </div>
-    </>
-  );
-}
