081eacec793f0656238517985dce59b820a12e39
Matt Robitaille <matt@anchour.com>
2023-06-08 16:08:27 -0400

    use Basepage template

--- src/scripts/pages/ProductPage.tsx
+++ src/scripts/pages/ProductPage.tsx
@@ -1,26 +1,19 @@
-import Footer from "../components/Footer"
-import Header from "../components/Header"
-import MailingListSignup from "../components/MailingListSignup"
 import ProductForm from "../components/product/Form"
 import ProductGallery from "../components/product/Gallery"
+import BasePage from "./Base"
 
 interface ProductPageProps {
 }
 
 const ProductPage = (props: ProductPageProps) => {
-  return <>
-    <Header showSuperHeader disableCopy />
-
+  return <BasePage>
     <section className="page-section">
       <div className="container product-container">
         <ProductGallery />
         <ProductForm />
       </div>
     </section>
-
-    <MailingListSignup />
-    <Footer />
-  </>
+  </BasePage>
 }
 
 export default ProductPage
