8ce9bc7e125f760ec3702bcfec30c8e121dd598b
Matt Robitaille <matt@anchour.com>
2023-07-11 13:27:57 -0400

    Switch to PHP from HTML to make this process simpler.

---
+++ static/partials/mailing-list-signup.php
@@ -0,0 +1,22 @@
+<!-- Mailing list signup -->
+<section class="page-section mailing-list-signup bg-primary-smoke color-primary-bone">
+  <div class="container">
+    <div class="mailing-list-signup__content">
+      <h2 class="mailing-list-signup__title typography-heading-lg">
+        Dive into a world of wonder
+      </h2>
+    </div>
+    <div class="mailing-list-signup__form-wrapper">
+      <div class="mailing-list-signup__form-outer">
+        <p class="typography-body-sm">
+          Get info about new releases, promotions, and updates right to your inbox — never be in
+          the dark about what’s new and next.
+        </p>
+        <form method="POST" action="" class="mailing-list">
+          <label for="search-term" class="sr-only">Search</label><input type="search" name="search-term" placeholder="Your email" class="mailing-list__input" /><button type="submit" class="btn btn--primary mailing-list__submit">Sign up</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- End mailing list signup -->
